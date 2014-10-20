341ce9a47726dbca713c88a8eff28ba12b406fcd
MiloIgor <igor.milovidoff@orange.fr>
2014-10-20 15:35:02 +0200

    Seconde modif branche igor

--- src/com/geojir/MainActivity.java
+++ src/com/geojir/MainActivity.java
@@ -19,8 +19,10 @@ public class MainActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
-        //Test de push de Igor
-        //second comment
+        //modif igor
+        boolean test = true;
+        if(test== false)
+        	return false;
         return true;
     }
 
