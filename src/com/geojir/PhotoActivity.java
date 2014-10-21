db3fcc5599b8bf9f958e9c2234b37a26836e4f57
MiloIgor <igor.milovidoff@orange.fr>
2014-10-21 11:52:38 +0200

    Fin insertion de code - Début debug

---
+++ src/com/geojir/PhotoActivity.java
@@ -0,0 +1,118 @@
+package com.geojir;
+
+import android.app.Activity;
+import android.graphics.Camera;
+import android.graphics.PixelFormat;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class PhotoActivity extends Activity implements SurfaceHolder.Callback
+{
+	private Camera camera;
+	private SurfaceView surfaceCamera;
+	private Boolean isPreview;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_photo);
+
+	    // Nous mettons l'application en plein écran et sans barre de titre
+	    getWindow().setFormat(PixelFormat.TRANSLUCENT);
+	    requestWindowFeature(Window.FEATURE_NO_TITLE);
+	    getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+	            WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+	    isPreview = false;
+
+	    // Nous appliquons notre layout
+	    setContentView(R.layout.activity_photo);
+
+	    // Nous récupérons notre surface pour le preview
+	    surfaceCamera = (SurfaceView) findViewById(R.id.surfaceViewCamera);
+
+	    // Méthode d'initialisation de la caméra
+	    InitializeCamera();
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.photo, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	public void surfaceChanged(SurfaceHolder arg0, int arg1, int arg2, int arg3) {
+	    // Nous prenons le contrôle de la camera
+//	    if (camera == null)
+//	        camera = Camera.open();
+	}
+
+	@Override
+	public void surfaceCreated(SurfaceHolder arg0) {
+	    // Nous arrêtons la camera et nous rendons la main
+	    if (camera != null) {
+//	        camera.stopPreview();
+	        isPreview = false;
+//	        camera.release();
+	    }
+	}
+
+	
+	public void InitializeCamera() 
+	{
+		// Nous attachons nos retours du holder à notre activité
+		surfaceCamera.getHolder().addCallback(this);
+		
+		// Nous spécifiions le type du holder en mode SURFACE_TYPE_PUSH_BUFFERS
+/*
+		surfaceCamera.getHolder().setType(
+		SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+*/
+	}
+	
+	public void surfaceDestroyed(SurfaceHolder holder) {
+	    // Nous arrêtons la camera et nous rendons la main
+	    if (camera != null) {
+//	        camera.stopPreview();
+	        isPreview = false;
+//	        camera.release();
+	    }
+	}
+	
+	// Retour sur l'application
+	@Override
+	public void onResume() {
+	    super.onResume();
+//	    camera = Camera.open();
+	}
+
+	// Mise en pause de l'application
+	@Override
+	public void onPause() {
+	    super.onPause();
+
+	    if (camera != null) {
+//	        camera.release();
+	        camera = null;
+	    }
+	}
+}
