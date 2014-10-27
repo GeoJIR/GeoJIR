344fd3333465faf160c591ae9d0a66af4db2fb7e
Jerome <motofondue@gmx.fr>
2014-10-27 14:07:43 +0100

    Drawable boutons

---
+++ src/com/geojir/AccountActivity.java
@@ -0,0 +1,12 @@
+package com.geojir;
+
+import android.os.Bundle;
+
+public class AccountActivity extends ParentMenuActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_account);
+	}
+}
