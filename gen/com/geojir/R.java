3b756b315ae9d322f60199ce23bd3e7104b0bf51
MiloIgor <igor.milovidoff@orange.fr>
2014-10-31 15:17:24 +0100

    Add directory values-fr

--- gen/com/geojir/R.java
+++ gen/com/geojir/R.java
@@ -17,6 +17,7 @@ public final class R {
          (such as screen margins) for screens with more than 820dp of available width. This
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
+ Default screen margins, per the Android Design guidelines. 
          */
         public static final int activity_horizontal_margin=0x7f040000;
         public static final int activity_vertical_margin=0x7f040001;
@@ -32,26 +33,29 @@ public final class R {
         public static final int acountcreation_connect=0x7f080002;
         public static final int acountcreation_email=0x7f080000;
         public static final int acountcreation_password=0x7f080001;
-        public static final int action_settings=0x7f080016;
-        public static final int content_frame=0x7f08000d;
-        public static final int drawable_account=0x7f080012;
-        public static final int drawable_around=0x7f080010;
-        public static final int drawable_capture=0x7f08000f;
-        public static final int drawable_follow=0x7f080011;
-        public static final int drawer_layout=0x7f08000c;
-        public static final int filterMonochrome=0x7f080006;
-        public static final int icon=0x7f080013;
-        public static final int imageApercu=0x7f080005;
-        public static final int imagePhotos=0x7f080003;
-        public static final int lateral_menu_left=0x7f08000e;
-        public static final int listViewMedias=0x7f08000a;
-        public static final int pathFileName=0x7f080014;
-        public static final int playbutton=0x7f080009;
-        public static final int recordbutton=0x7f080008;
-        public static final int remark=0x7f080015;
-        public static final int savefilterbutton=0x7f080007;
-        public static final int textView1=0x7f080004;
-        public static final int textViewListMedia=0x7f08000b;
+        public static final int action_settings=0x7f080019;
+        public static final int content_frame=0x7f080010;
+        public static final int drawable_account=0x7f080015;
+        public static final int drawable_around=0x7f080013;
+        public static final int drawable_capture=0x7f080012;
+        public static final int drawable_follow=0x7f080014;
+        public static final int drawer_layout=0x7f08000f;
+        public static final int editText1=0x7f080004;
+        public static final int editText2=0x7f080006;
+        public static final int filterMonochrome=0x7f080009;
+        public static final int icon=0x7f080016;
+        public static final int imageApercu=0x7f080008;
+        public static final int imagePhotos=0x7f080007;
+        public static final int lateral_menu_left=0x7f080011;
+        public static final int listViewMedias=0x7f08000d;
+        public static final int pathFileName=0x7f080017;
+        public static final int playbutton=0x7f08000c;
+        public static final int recordbutton=0x7f08000b;
+        public static final int remark=0x7f080018;
+        public static final int savefilterbutton=0x7f08000a;
+        public static final int textView1=0x7f080005;
+        public static final int textViewFollowers=0x7f080003;
+        public static final int textViewListMedia=0x7f08000e;
     }
     public static final class layout {
         public static final int activity_account=0x7f030000;
@@ -79,20 +83,22 @@ public final class R {
         public static final int drawable_capture=0x7f05000b;
         public static final int drawable_follow=0x7f05000d;
         public static final int email=0x7f050014;
+        public static final int followedsList=0x7f050017;
+        public static final int followersList=0x7f050016;
         public static final int hello_world=0x7f050001;
         public static final int ic_camera_description=0x7f050005;
         public static final int ic_medias_description=0x7f050007;
         public static final int ic_micro_description=0x7f050006;
         public static final int ic_photo_description=0x7f050004;
         public static final int ic_photo_label=0x7f050008;
-        public static final int infoAccount=0x7f050016;
-        public static final int infoSaved=0x7f050017;
+        public static final int infoAccount=0x7f050018;
+        public static final int infoSaved=0x7f050019;
         public static final int play_song=0x7f05000a;
         public static final int save_song=0x7f050009;
         public static final int title_activity_account=0x7f050011;
         public static final int title_activity_around=0x7f05000f;
         public static final int title_activity_follow=0x7f050010;
-        public static final int title_activity_list_media=0x7f050018;
+        public static final int title_activity_list_media=0x7f05001a;
         public static final int username=0x7f050012;
         public static final int youremail=0x7f050015;
         public static final int yourname=0x7f050013;
@@ -118,9 +124,20 @@ public final class R {
         res/values-v11/styles.xml on API 14+ devices.
     
  API 14 theme customizations can go here. 
+
+        Base application theme, dependent on API level. This theme is replaced
+        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
+    
+
+            Theme customizations available in newer API levels can go in
+            res/values-vXX/styles.xml, while customizations related to
+            backward-compatibility can go here.
+        
          */
         public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
+ All customizations that are NOT specific to a particular API-level can go here. 
+ Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f060001;
