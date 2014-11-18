815c594e644698a343a35583e631f7baab5dad6a
MiloIgor <igor.milovidoff@orange.fr>
2014-11-18 10:24:28 +0100

    Presence Google Play Service

--- src/com/geojir/ServletActivity.java
+++ src/com/geojir/ServletActivity.java
@@ -17,7 +17,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class ServletActivity extends ParentMenuActivity
 {
